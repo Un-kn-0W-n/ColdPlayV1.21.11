c3cb7cc6e34569493f9896d53516e433bffc2d0c
Un-kn-0W-n <unknownpersonhere10@gmail.com>
2026-09-16 19:05:11 +0300

    BackTrack had really poor logic

--- src/main/java/cn/timer/coldplay/client/mixin/LivingEntityRendererMixin.java
+++ src/main/java/cn/timer/coldplay/client/mixin/LivingEntityRendererMixin.java
@@ -2,7 +2,6 @@ package cn.timer.coldplay.client.mixin;
 
 import cn.timer.coldplay.client.ClientCore;
 import cn.timer.coldplay.client.manager.RotationManager;
-import cn.timer.coldplay.client.module.impl.combat.BackTrack;
 import cn.timer.coldplay.client.module.impl.visuals.EntityESP;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.client.Minecraft;
@@ -12,7 +11,6 @@ import net.minecraft.client.renderer.rendertype.RenderType;
 import net.minecraft.client.renderer.rendertype.RenderTypes;
 import net.minecraft.resources.Identifier;
 import net.minecraft.util.Mth;
-import net.minecraft.util.ARGB;
 import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -53,19 +51,6 @@ abstract class LivingEntityRendererMixin {
     private RenderType coldplay$chamsMaterial(RenderType original, LivingEntityRenderState state) {
         EntityESP entityEsp = ClientCore.get().entityEsp();
         return entityEsp != null && entityEsp.usesChams() && state.getData(EntityESP.COLOR) != null
-                && state.getData(BackTrack.REAL_COLOR) == null
                 ? RenderTypes.textSeeThrough(getTextureLocation(state)) : original;
-    }
-
-    @ModifyExpressionValue(
-            method = "submit(Lnet/minecraft/client/renderer/entity/state/LivingEntityRenderState;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/SubmitNodeCollector;Lnet/minecraft/client/renderer/state/CameraRenderState;)V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/entity/LivingEntityRenderer;getModelTint(Lnet/minecraft/client/renderer/entity/state/LivingEntityRenderState;)I")
-    )
-    private int coldplay$chamsTint(int original, LivingEntityRenderState state) {
-        Integer backTrackColor = state.getData(BackTrack.REAL_COLOR);
-        if (backTrackColor != null) {
-            return ARGB.multiply(original, backTrackColor);
-        }
-        return original;
     }
 }
